ca3e52f6cd5bfd379dde3804fa23ef9d3c866433
Jose Álvarez <iphone4jose@icloud.com>
2021-06-11 09:55:08 +0200

    add navigation view

--- app/src/main/java/es/juanTejada/TipCalculator/ui/main/TipViewModel.kt
+++ app/src/main/java/es/juanTejada/TipCalculator/ui/main/TipViewModel.kt
@@ -1,7 +1,12 @@
 package es.juanTejada.TipCalculator.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TipViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _bill: MutableLiveData<Float> = MutableLiveData(0.00f)
+    private val bill: LiveData<Float>
+    get() = _bill
+    //TODO
 }
